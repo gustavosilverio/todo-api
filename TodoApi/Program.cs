6fa30e4821ffe2e1ea0d3f40afa7c10ecc1de1c9
GustavoSilverio <gustavosk290905@gmail.com>
2025-09-14 19:14:46 -0300

    fix: swagger environment

--- TodoApi/Program.cs
+++ TodoApi/Program.cs
@@ -28,7 +28,7 @@ var app = builder.Build();
 
 app.UseExceptionHandler();
 
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
